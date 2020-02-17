292948ecb96e62cc706070acc4b5b5127004e0d8
VladuxRTK <33584762+VladuxRTK@users.noreply.github.com>
2020-02-17 17:08:25 +0200

    Added enemy abstract class

---
+++ Assets/Scripts/Enemies.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Enemies : MonoBehaviour
+{
+
+    public int health;
+    public int damage;
+    public float timeUntillIncrease;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void IncreaseAttributes(int damage)
+    {
+        if (Time.time >= timeUntillIncrease)
+        {
+            timeUntillIncrease += 300f;
+            this.damage += damage;
+        }
+    }
+    public void IncreaseAttributes(int damage,int health)
+    {
+        if (Time.time >= timeUntillIncrease)
+        {
+            this.damage += damage;
+            this.health += health;
+        }
+    }
+}
