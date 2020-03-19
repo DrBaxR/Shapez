7b5cbb0eb69c37b0149fd52b6668b04c25fe78ee
VladuxRTK <33584762+VladuxRTK@users.noreply.github.com>
2020-03-19 18:04:29 +0200

    started making the ui

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class UIManager : MonoBehaviour
+{
+
+    public RectTransform mainMenu,titleMenu;
+    // Start is called before the first frame update
+    void Start()
+    {
+        titleMenu.DOAnchorPos(Vector2.zero, 0.5f);
+        mainMenu.DOAnchorPos(Vector2.zero, 1f);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
