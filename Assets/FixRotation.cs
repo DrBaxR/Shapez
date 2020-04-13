35a8caaceba9732931d6e1fb835ab7638ca84960
VladuxRTK <33584762+VladuxRTK@users.noreply.github.com>
2020-04-13 19:44:51 +0300

    added laser skill(work in progress)

---
+++ Assets/FixRotation.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FixRotation : MonoBehaviour
+{
+    public float whatPosX;
+    public float whatPosY;
+
+    private Transform player;
+
+
+    private void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+    private void Update()
+    {
+        this.transform.position = new Vector3(player.position.x + whatPosX,player.position.y + whatPosY,player.position.z);
+    }
+
+    void LateUpdate()
+    {
+        
+    }
+}
---
+++ Assets/Laser.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+
+    public Transform hitPoint;
+
+    private LineRenderer laser;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        laser = GetComponent<LineRenderer>();
+        laser.enabled = false;
+        laser.useWorldSpace = true;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(this.transform.position, hitPoint.position);
+        if(hit)
+        {
+            Debug.Log(hit.transform.tag);
+        }
+        laser.SetPosition(0, this.transform.position);
+        laser.SetPosition(1, hitPoint.transform.position);
+        if (Input.GetKey(KeyCode.R))
+        {
+            StartCoroutine(LaserShot(hit));
+        }
+      
+    }
+
+    private IEnumerator LaserShot(RaycastHit2D hit)
+    {
+
+
+        laser.enabled = true;
+        if (hit)
+        {
+            if (hit.transform.tag!="Player")
+            {
+                Destroy(hit.collider.gameObject);
+            }
+        }
+        yield return new WaitForSeconds(2f);
+        laser.enabled = false;
+        yield return new WaitForSeconds(2f);
+        laser.enabled = true;
+        if (hit)
+        {
+            if (hit.transform.tag!="Player")
+            {
+                Destroy(hit.collider.gameObject);
+            }
+        }
+        yield return new WaitForSeconds(2f);
+        laser.enabled = false;
+
+    }
+}
