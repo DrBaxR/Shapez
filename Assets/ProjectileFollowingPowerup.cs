f019019ab5e52736ab3fb8884eb991e1fc5e4a05
VladuxRTK <33584762+VladuxRTK@users.noreply.github.com>
2020-07-16 12:44:38 +0300

    started working on a new projectile follower powerup

---
+++ Assets/ProjectileFollowingPowerup.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileFollowingPowerup : MonoBehaviour
+{
+    private Player player;
+    private List<Transform> targets;
+    public List<GameObject> projetiles;
+
+
+    [HideInInspector]
+    public bool isActive;
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isActive = false;
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
